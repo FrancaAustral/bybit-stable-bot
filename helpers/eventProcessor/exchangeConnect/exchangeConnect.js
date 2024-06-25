d120fddace5072d2047a366c932a60c738b5d18e
checho651 <maquieiraezequiel@gmail.com>
2024-06-25 12:11:18 -0300

    Add limit orders management. Store & remove.

--- helpers/eventProcessor/exchangeConnect/exchangeConnect.js
+++ helpers/eventProcessor/exchangeConnect/exchangeConnect.js
@@ -43,6 +43,7 @@ class XchgConnect {
     this.ordebook = { bid: [], ask: [] }
     this.candles = []
     this.tradingInfo = {}
+    this.closeOrder = null
 
     this.lastCandleMsgMts = 0
     this.lastOrdebookMsgMts = 0
@@ -85,6 +86,73 @@ class XchgConnect {
 
   manageWalletMsg (msg) {
     this.setWallet(msg.data[0])
+  }
+
+  storeNewLimitOrder (order, type) {
+    this[type] = order
+    logger('log', true, 'ON', order)
+  }
+
+  async storeLimitOrder (order, type) {
+    const actualOrder = this[type]
+    if (!actualOrder) return this.storeNewLimitOrder(order, type)
+    if (actualOrder.orderId !== order.orderId) {
+      await this.cancelLimitOrder(actualOrder)
+      return this.storeNewLimitOrder(order, type)
+    }
+    this[type] = order
+    if (JSON.stringify(actualOrder) !== JSON.stringify(this[type])) {
+      logger('log', true, 'OU:', order)
+    }
+  }
+
+  removeLimitOrder (order, type) {
+    if (this[type]) delete this[type]
+    logger('log', true, 'OC:', order)
+  }
+
+  isCloseOrder (orderStatus) {
+    // Bot not using Conditional order, in that case add 'Untriggered'
+    return !['New', 'PartiallyFilled'].includes(orderStatus)
+  }
+
+  manageOrderMsg (msg) {
+    const order = msg.data[0]
+    const { category, orderType, orderStatus } = order
+    if (category !== 'spot' || orderType !== 'Limit') return false
+    return (this.isCloseOrder(orderStatus))
+      ? this.removeLimitOrder(order, 'closeOrder')
+      : this.storeLimitOrder(order, 'closeOrder')
+  }
+
+  async getLimitOders () {
+    const params = {
+      category: 'spot',
+      symbol: this.pair,
+      openOnly: 0, // Only returns open orders.
+      limit: 50
+    }
+    const ordersResponse = await this.rest.getOrders(params)
+    return ordersResponse.result.list.map((o) => {
+      return { category: 'spot', ...o } // Response missing 'category'.
+    })
+  }
+
+  async updateLimitOrders () {
+    const orders = await this.getLimitOders()
+    if (orders.length > 1) {
+      const canceled = await this.rest.cancelAllOrders({
+        category: 'spot',
+        symbol: this.pair
+      })
+      return this.logger(
+        'log',
+        true,
+        'Canceled multiple limit orders:',
+        canceled.result.list.map((o) => o.orderId)
+      )
+    }
+    orders.forEach((o) => this.storeLimitOrder(o, 'closeOrder'))
   }
 
   storeCandle (candle) {
